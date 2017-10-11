688aae68e60c72c4a5f2c2073ac04b24496aa3b7
qst0 <masondyoung@gmail.com>
2017-10-11 13:57:52 -0700

    Updated

---
+++ DetailViewController.swift
@@ -0,0 +1,65 @@
+//
+//  DetailViewController.swift
+//  MetaHobby_image_viewer
+//
+//  Created by qst0 on 9/4/17.
+//  Copyright © 2017 qst0. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = selectedImage
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        //let myImg: UIImage = imageView.image!
+        let activityItem: [AnyObject] = [self.imageView.image as AnyObject]
+        let vc = UIActivityViewController(activityItems: activityItem as [AnyObject], applicationActivities: nil)
+        //print ("Hoop hoop howdy", type(of: imageView.image!))
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MetaHobby_image_viewer/ViewController.swift
+++ MetaHobby_image_viewer/ViewController.swift
@@ -8,11 +8,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        title = "MetaHobby Image Viewer"
+
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("metaguy") {
+                pictures.append(item)
+                //Metaguy!
+            }
+        }
+        
+        print(pictures)
+        print("🌴")
+        print(items)
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //load the deatil view controller typecasting it to a be a detailviewcontroller
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            // set the selected image property of the vc
+            vc.selectedImage = pictures[indexPath.row]
+            // now push it onto the naviagtion controller
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
